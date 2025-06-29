dd3d20e9cd4abfb6ce315c2b8aacb8b60fc5f35a
callerbalogun <callerbalogun@gmail.com>
2025-06-29 18:05:14 +0100

    updated keyboard to avoid view

--- App.js
+++ App.js
@@ -2,18 +2,21 @@ import "react-native-gesture-handler";
 import { PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { View } from "react-native";
 import StackChat from "./components/StackedScreens";
 
 export default function App() {
   return (
     <PaperProvider>
-      <NavigationContainer>
-        <View className="flex-1">
-          <StatusBar style="light" />
-          <StackChat />
-        </View>
-      </NavigationContainer>
+      <SafeAreaView style={{ flex: 1 }} edges={["bottom"]}>
+        <NavigationContainer>
+          <View className="flex-1">
+            <StatusBar style="light" />
+            <StackChat />
+          </View>
+        </NavigationContainer>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
--- screens/DmScreen.js
+++ screens/DmScreen.js
@@ -1,42 +1,67 @@
 import {
   FlatList,
-  SafeAreaView,
   KeyboardAvoidingView,
   View,
   TouchableWithoutFeedback,
-  Keyboard,
   Platform,
+  Keyboard,
 } from "react-native";
+import { useHeaderHeight } from "@react-navigation/elements";
 import DateSurface from "../components/DateSurface";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ChatSurface from "../components/ChatSurface";
 import ChatInput from "../components/ChatInput";
 
 const DmScreen = () => {
   const [message, setMessage] = useState([]);
+  const [flexToggle, setFlexToggle] = useState(false);
+  useEffect(() => {
+    const keyboardShowListener = Keyboard.addListener("keyboardDidShow", () => {
+      setFlexToggle(false);
+    });
+
+    const keyboardHideListener = Keyboard.addListener("keyboardDidHide", () => {
+      setFlexToggle(true);
+    });
+
+    return () => {
+      keyboardShowListener.remove();
+      keyboardHideListener.remove();
+    };
+  }, []);
+
   const sendMessageHandler = (text, time, isUser) => {
     setMessage((prev) => [...prev, { text: text, time: time, isUser: true }]);
   };
 
+  const headerHeight = useHeaderHeight();
+
   return (
-    <KeyboardAvoidingView className="flex-1" behavior="height">
-      <View className="flex-1 px-2 bg-primary-100">
-        <DateSurface />
-        <FlatList
-          className="flex-1 mb-2"
-          showsVerticalScrollIndicator={false}
-          showsHorizontalScrollIndicator={false}
-          data={message}
-          renderItem={({ item, index }) => (
-            <>
-              <ChatSurface isUser={item.isUser} time={item.time} key={index}>
-                {item.text}
-              </ChatSurface>
-            </>
-          )}
-        />
-        <ChatInput sendHandler={sendMessageHandler} />
-      </View>
+    <KeyboardAvoidingView
+      style={flexToggle ? [{ flexGrow: 1 }] : [{ flex: 1 }]}
+      enabled={!flexToggle}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 60 : headerHeight}
+    >
+      <TouchableWithoutFeedback>
+        <View className="flex-1 px-2 bg-primary-100">
+          <DateSurface />
+          <FlatList
+            className="flex-1 mb-2"
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            data={message}
+            renderItem={({ item, index }) => (
+              <>
+                <ChatSurface isUser={item.isUser} time={item.time} key={index}>
+                  {item.text}
+                </ChatSurface>
+              </>
+            )}
+          />
+          <ChatInput sendHandler={sendMessageHandler} />
+        </View>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 };
